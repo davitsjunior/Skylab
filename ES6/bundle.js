c01b354a1d3ca84f5e92cbc96f888a064fc50b93
davitsjunior <dv_junior@hotmail.com>
2020-04-02 13:32:46 -0300

    ES6 segundo envio

--- ES6/bundle.js
+++ ES6/bundle.js
@@ -29,3 +29,59 @@ var MinhaLista = new TodoList();
 document.getElementById('novoTodo').onclick = function () {
   MinhaLista.addTodo();
 };
+
+var arr = [1, 3, 4, 5, 8, 9];
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum);
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter);
+var find = arr.find(function (item) {
+  return item == 4;
+});
+console.log(find); //arrow functions
+
+var arr2 = [1, 3, 4, 5, 8, 9];
+var newArr1 = arr.map(function (item) {
+  return item + 2;
+});
+console.log(newArr1); //valores padrão
+
+var soma = function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
+};
+
+console.log(soma(1));
+console.log(soma()); //desestruturação
+
+var usuario = {
+  nome: 'Davi',
+  idade: 35,
+  endereco: {
+    cidade: 'Borrazópolis',
+    estado: 'Paraná'
+  }
+};
+var nome = usuario.nome,
+    idade = usuario.idade,
+    cidade = usuario.endereco.cidade;
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+function mostraNome(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  console.log(nome, idade);
+}
+
+mostraNome(usuario); //Operadores Rest/Spread
--- ES6/main.js
+++ ES6/main.js
@@ -1,4 +1,5 @@
 class TodoList{
+
     constructor(){
         this.todos = [];
     }
@@ -15,3 +16,68 @@ document.getElementById('novoTodo').onclick = function(){
     MinhaLista.addTodo();
 }
 
+
+const arr = [1, 3, 4, 5, 8, 9];
+
+const newArr = arr.map(function(item, index){
+    return item + index;
+});
+
+console.log(newArr)
+
+const sum = arr.reduce(function(total, next){
+    return total + next;
+});
+
+console.log(sum);
+
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+});
+
+console.log(filter);
+
+const find = arr.find(function(item){
+    return item == 4;
+});
+
+console.log(find);
+
+//arrow functions
+
+const arr2 = [1, 3, 4, 5, 8, 9];
+
+const newArr1 = arr.map(item => item + 2);
+
+console.log(newArr1);
+
+//valores padrão
+
+const  soma = (a=3,b=6) => a + b;
+
+console.log(soma(1));
+console.log(soma());
+
+//desestruturação
+
+const usuario = {
+    nome: 'Davi',
+    idade: 35,
+    endereco:{
+        cidade: 'Borrazópolis',
+        estado: 'Paraná',
+    },
+};
+
+const {nome, idade, endereco:{cidade}} = usuario;
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+function mostraNome({nome, idade}){
+    console.log(nome, idade);
+}
+
+mostraNome(usuario);
+
+//Operadores Rest/Spread
